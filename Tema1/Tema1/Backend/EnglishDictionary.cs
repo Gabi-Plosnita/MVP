95fed9e55a7699eb7caa51084f699a11da53154b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-03-13 18:43:02 +0200

    Create EnglishDictionary.cs

---
+++ Tema1/Tema1/Backend/EnglishDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Dictionar.Backend
+{
+    public class EnglishDictionary
+    {
+        public Dictionary<string, List<Word>> dictionaryByCategory {  get; set; }
+
+        public Dictionary<string, List<Word>> dictionaryByLetter { get; set; }
+
+
+
+    }
+}
