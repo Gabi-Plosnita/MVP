c45b0677593adb19f36738621c317c4b6a0c18db
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-03-13 18:27:13 +0200

    Create word class

---
+++ Tema1/Tema1/Backend/Word.cs
@@ -0,0 +1,13 @@
+﻿namespace Dictionar.Backend
+{
+    public class Word
+    {
+        public string name {  get; set; }
+
+        public string description { get; set; }
+
+        public string category { get; set; }
+
+        public string image { get; set; }
+    }
+}
