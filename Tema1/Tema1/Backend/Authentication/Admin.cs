21a18624ec004e1e5c7c88b426ad1eff7d6e6f07
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2024-03-16 14:03:59 +0200

    Create Admin and Authentication class

---
+++ Tema1/Tema1/Backend/Authentication/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace Tema1.Backend
+{
+    public class Admin
+    {
+        public string Name { get; private set; }
+
+        public string Password { get; private set; }
+    }
+}
---
+++ Tema1/Tema1/Backend/Authentication/Authentication.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tema1.Backend
+{
+    public class Authentication
+    {
+        public List<Admin> Admins { get; private set; }
+
+        public void LoadFromFile(string filePath)
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(filePath);
+
+                Admins = JsonConvert.DeserializeObject<List<Admin>>(jsonData);
+
+                Console.WriteLine("Data loaded successfully from file.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("Error deserializing JSON data.");
+            }
+        }
+    }
+}
