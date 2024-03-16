a937a7f9d20f0d199d2e5be34f80d10e7ba90591
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2024-03-16 14:23:06 +0200

    Create constructors for Word

--- Tema1/Tema1/Backend/Dictionary/Word.cs
+++ Tema1/Tema1/Backend/Dictionary/Word.cs
@@ -9,5 +9,29 @@
         public string Category { get; set; }
 
         public string Image { get; set; }
+
+        public Word() 
+        {
+            Name = "None";
+            Description = "None";
+            Category = "None";
+            Image = "C:\\Gabi\\Mvp\\MVP\\Tema1\\Tema1\\Resources\\Images\\NoImage.jpg";
+        }
+
+        public Word(string name, string category, string description)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            Image = "C:\\Gabi\\Mvp\\MVP\\Tema1\\Tema1\\Resources\\Images\\NoImage.jpg";
+        }
+
+        public Word(string name, string category, string description, string image)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            Image = image;
+        }
     }
 }
