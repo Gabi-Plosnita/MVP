7b2bdcfaf6914a061cee995c4e3cdcea2d90c10d
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-08 12:37:37 +0300

    Create Piece class

---
+++ Checkers_Game/Checkers_Game/Model/Piece.cs
@@ -0,0 +1,36 @@
+﻿namespace Checkers_Game.Model
+{
+    public class Piece : BaseNotification
+    {
+
+        private EType piceType;
+
+        private EColor pieceColor;
+
+        public Piece(EType type, EColor color)
+        {
+            piceType = type;
+            pieceColor = color;
+        }
+
+        public EType PieceType
+        {
+            get { return piceType; }
+            set
+            {
+                piceType = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public EColor PieceColor
+        {
+            get { return pieceColor; }
+            set
+            {
+                pieceColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
