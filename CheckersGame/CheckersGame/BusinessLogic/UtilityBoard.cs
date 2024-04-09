8c7e2341a9127307245007d0a3d649e59d3b5cd4
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-09 12:34:31 +0300

    Create static class UtilityBoard
    Also create IsPositionInBoard method

---
+++ CheckersGame/CheckersGame/BusinessLogic/UtilityBoard.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckersGame.BusinessLogic
+{
+    public static class UtilityBoard
+    {
+        static public bool IsPositionInBoard(Position position, int nrRows, int nrCols)
+        {
+            return position.Row >= nrRows && position.Row < nrRows && position.Col >= nrCols && position.Col < nrCols;
+        }
+    }
+}
