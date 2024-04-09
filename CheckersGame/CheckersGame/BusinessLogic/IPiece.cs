d7493831118a19b3461ce3100854ea240470465e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-09 11:19:32 +0300

    Create IPiece.cs

---
+++ CheckersGame/CheckersGame/BusinessLogic/IPiece.cs
@@ -0,0 +1,17 @@
+﻿using CheckersGame.DataAccess;
+using System.ComponentModel;
+
+namespace CheckersGame.BusinessLogic
+{
+    public interface IPiece : INotifyPropertyChanged
+    {
+        EType GetType();
+
+        EColor GetColor();
+
+        void SetType(EType type);
+
+        void SubscribeToPropertyChanged(PropertyChangedEventHandler handler);
+
+    }
+}
