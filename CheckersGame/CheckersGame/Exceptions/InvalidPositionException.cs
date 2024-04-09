1695363912a5020066f1ae5cd141abc1ade36724
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-09 12:36:11 +0300

    Create InvalidPositionException.cs

---
+++ CheckersGame/CheckersGame/Exceptions/InvalidPositionException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CheckersGame.Exceptions
+{
+    public class InvalidPositionException : Exception
+    {
+        public InvalidPositionException() : base() { }
+        public InvalidPositionException(string message) : base(message) { }
+    }
+}
