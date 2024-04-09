da65287da0d87161236275413ce02eafbacc7dc8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-09 12:09:43 +0300

    Create Piece class

---
+++ CheckersGame/CheckersGame/BusinessLogic/Piece.cs
@@ -0,0 +1,44 @@
+﻿using CheckersGame.DataAccess;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+namespace CheckersGame.BusinessLogic
+{
+    public abstract class Piece : BaseNotification
+    {
+
+        private EType pieceType;
+
+        private EColor pieceColor;
+
+        protected Piece(EType type, EColor color)
+        {
+            pieceType = type;
+            pieceColor = color;
+        }
+
+        public EType PieceType
+        {
+            get { return pieceType; }
+            set
+            {
+                pieceType = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public EColor PieceColor
+        {
+            get { return pieceColor; }
+        }
+
+        public void SubscribeToPropertyChanged(PropertyChangedEventHandler handler)
+        {
+            PropertyChanged += handler;
+        }
+
+        public abstract List<Position> GetPossibleMoves(Piece[,] board);
+
+    }
+}
