3c19ccd2c9f06ebd049fac5ca60cb4927c7f1bd3
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-13 19:20:57 +0300

    Create InvalidMoveException.cs

---
+++ CheckersGame/CheckersGame/Exceptions/InvalidMoveException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CheckersGame.Exceptions
+{
+    public class InvalidMoveException : Exception
+    {
+        public InvalidMoveException() : base() { }
+        public InvalidMoveException(string message) : base(message) { }
+    }
+}
