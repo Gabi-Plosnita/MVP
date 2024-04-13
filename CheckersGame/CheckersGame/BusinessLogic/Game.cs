792024b1b842538f138c47f51e5acf5ca3c8f2c8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-13 19:42:26 +0300

    Implement MakeMove method for Game class

--- CheckersGame/CheckersGame/BusinessLogic/Game.cs
+++ CheckersGame/CheckersGame/BusinessLogic/Game.cs
@@ -1,4 +1,6 @@
 ﻿using CheckersGame.DataAccess;
+using CheckersGame.Exceptions;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace CheckersGame.BusinessLogic
@@ -66,6 +68,55 @@ namespace CheckersGame.BusinessLogic
         public bool IsGameOver()
         {
             return RedPiecesNr == 0 || BlackPiecesNr == 0;
+        }
+
+        public void MakeMove(Position startPos, Position endPos)
+        {
+            if (!UtilityBoard.IsPositionInBoard(startPos, board.Count, board[0].Count))
+            {
+                throw new InvalidPositionException($"Position {startPos.ToString()} is invalid!");
+            }
+            if (!UtilityBoard.IsPositionInBoard(endPos, board.Count, board[0].Count))
+            {
+                throw new InvalidPositionException($"Position {endPos.ToString()} is invalid!");
+            }
+
+            Piece piece = board[startPos.Row][startPos.Col];
+            if (piece.PieceColor != turn)
+            {
+                throw new InvalidPieceColorException("This is not your piece!");
+            }
+
+            List<Position> possibleMoves = piece.GetPossibleMoves(startPos, board);
+            if (!possibleMoves.Contains(endPos))
+            {
+                throw new InvalidMoveException($"Invalid move from {startPos.ToString()} to {endPos.ToString()}");
+            }
+
+            board[endPos.Row][endPos.Col] = board[startPos.Row][startPos.Col];
+            board[startPos.Row][startPos.Col] = new Piece(EType.None, EColor.None);
+
+            if (System.Math.Abs(startPos.Row - endPos.Row) == 2)
+            {
+                Position jumpedPiece = new Position((startPos.Row + endPos.Row) / 2, (startPos.Col + endPos.Col) / 2);
+                board[jumpedPiece.Row][jumpedPiece.Col] = new Piece(EType.None, EColor.None);
+                if (turn == EColor.Black)
+                {
+                    RedPiecesNr--;
+                }
+                else
+                {
+                    BlackPiecesNr--;
+                }
+            }
+
+            if (piece.PieceType == EType.NormalPiece)
+            {
+                if (endPos.Row == 0 || endPos.Row == 7)
+                {
+                    board[endPos.Row][endPos.Col].PieceType = EType.QueenPiece;
+                }
+            }
         }
 
     }
