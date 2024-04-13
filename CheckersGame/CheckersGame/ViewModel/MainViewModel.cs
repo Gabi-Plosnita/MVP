5f88be5d06974a16ec1add4609e7331ef52a84e8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-13 20:01:02 +0300

    Create MainViewModel.cs

---
+++ CheckersGame/CheckersGame/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CheckersGame.BusinessLogic;
+using System.Collections.ObjectModel;
+
+namespace CheckersGame.ViewModel
+{
+    public class MainViewModel
+    {
+        private Game game;
+        public ObservableCollection<ObservableCollection<Piece>> Board => game.Board;
+        public MainViewModel()
+        {
+            game = new Game();
+        }
+
+        
+    }
+}
