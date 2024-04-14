721cde8728364330603e4175ca03e7771c48038c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-14 14:13:22 +0300

    Update code in MainViewModel

--- CheckersGame/CheckersGame/ViewModel/MainViewModel.cs
+++ CheckersGame/CheckersGame/ViewModel/MainViewModel.cs
@@ -1,17 +1,50 @@
 ﻿using CheckersGame.BusinessLogic;
+using CheckersGame.Commands;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace CheckersGame.ViewModel
 {
     public class MainViewModel
     {
-        private Game game;
-        public ObservableCollection<ObservableCollection<Piece>> Board => game.Board;
+        private Game Game = new Game();
+
+        public ObservableCollection<Square> Board;
+
         public MainViewModel()
         {
-            game = new Game();
+            InitializeBoard();
+            SqaureClickCommand = new RelayCommand<Square>(SqaureClick);
         }
 
-        
+        private void InitializeBoard()
+        {
+            Board = new ObservableCollection<Square>();
+
+            for (int i = 0; i < Game.Board.Count; i++)
+            {
+                for (int j = 0; j < Game.Board[i].Count; j++)
+                {
+                    Square square = new Square();
+                    if(i + j % 2 != 0)
+                    {
+                        square.BackgroundImagePath = ".\\..\\..\\Resources\\bg1.png";
+                    }
+                    else
+                    {
+                        square.BackgroundImagePath = ".\\..\\..\\Resources\\bg2.png";
+                    }
+                    square.Piece = Game.Board[i][j];
+                    Board.Add(square);
+                }
+            }
+        }
+
+        public ICommand SqaureClickCommand { get; private set; }
+
+        private void SqaureClick(Square square)
+        {
+            square.ImagePath = ".\\..\\..\\Resources\\bg1.png";
+        }
     }
 }
