4f47445df0a8654dc718c65d8fbcc6106b5724bb
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-14 14:03:45 +0300

    Create Square class

---
+++ CheckersGame/CheckersGame/BusinessLogic/Square.cs
@@ -0,0 +1,24 @@
+﻿using CheckersGame.DataAccess;
+
+namespace CheckersGame.BusinessLogic
+{
+    public class Square : BaseNotification
+    {
+        public string BackgroundImagePath { get; set; }
+
+        public string ImagePath
+        {
+            get
+            {
+                return Piece.ImagePath;
+            }
+            set
+            {
+                Piece.ImagePath = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Piece Piece;
+    }
+}
