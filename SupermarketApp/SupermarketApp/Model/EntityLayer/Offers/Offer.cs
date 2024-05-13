0b9f0deb9c3150d6e2080f61a078dfca577db7df
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-13 20:28:35 +0300

    Create Offer entity

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Offers/Offer.cs
@@ -0,0 +1,21 @@
+﻿namespace SupermarketApp.Model.EntityLayer.Offers
+{
+    public class Offer
+    {
+        public int OfferId { get; set; }
+
+        public EOfferType OfferType { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public double OfferPercentage { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
