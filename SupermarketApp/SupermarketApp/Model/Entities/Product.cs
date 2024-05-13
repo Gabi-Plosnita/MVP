3966cf73296c3c646f7f979d56f2dae58e7950f1
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-13 08:56:32 +0300

    Create Product.cs

---
+++ SupermarketApp/SupermarketApp/Model/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace SupermarketApp.Model.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Barcode { get; set; }
+
+        public Category Category { get; set; }
+
+        public Supplier Supplier { get; set; }
+    }
+}
