5dc707ed6ec8b30ca910cdf519b02cdf2e80e3d7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-15 15:27:29 +0300

    Create a new migration for IsActive property

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Migrations/20240515122626_SetDefaultValueForIsActiveMigration.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupermarketApp.Model.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDefaultValueForIsActiveMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Suppliers",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Stocks",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Receipts",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "ProductReceipts",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Offers",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Suppliers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Stocks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Receipts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "ProductReceipts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Offers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+        }
+    }
+}
