ff80496fc362df48c46ea2d4de880ec0b223dfc1
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-15 15:45:52 +0300

    Create ICategoryRepository and CategoryRepository

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using SupermarketApp.Model.DataAccessLayer.DataContext;
+using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(SupermarketDbContext context) : base(context)
+        {
+        }
+
+        public List<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(int categoryId, Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetCategories();
+
+        Category GetCategory(int id);
+
+        void AddCategory(Category category);
+
+        void UpdateCategory(int categoryId, Category category);
+
+        void DeleteCategory(int id);
+    }
+}
