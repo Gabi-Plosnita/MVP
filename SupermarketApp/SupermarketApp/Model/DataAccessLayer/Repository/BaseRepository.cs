00c37936a123bc7635d22fcd6520e7d33b791a44
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-15 15:38:28 +0300

    Create BaseRepository.cs

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using SupermarketApp.Model.DataAccessLayer.DataContext;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public class BaseRepository
+    {
+        protected readonly SupermarketDbContext _context;
+
+        public BaseRepository(SupermarketDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
