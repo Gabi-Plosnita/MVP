d3e2e120c03db39e90e852c23d996d51b1807d57
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-15 17:35:56 +0300

    Create ISupplierRepository and SupplierRepository

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/SupplierRepository/ISupplierRepository.cs
@@ -0,0 +1,19 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public interface ISupplierRepository
+    {
+        List<Supplier> GetSuppliers();
+
+        Supplier GetSupplier(int id);
+
+        void AddSupplier(Supplier supplier);
+
+        void UpdateSupplier(int id, Supplier updatedSupplier);
+
+        void DeleteSupplier(int id);
+
+        List<Product> GetProductsFromSupplier(int id);
+    }
+}
---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/SupplierRepository/SupplierRepository.cs
@@ -0,0 +1,42 @@
+﻿using SupermarketApp.Model.DataAccessLayer.DataContext;
+using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public class SupplierRepository : BaseRepository, ISupplierRepository
+    {
+        public SupplierRepository(SupermarketDbContext context) : base(context)
+        {
+        }
+
+        public List<Supplier> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Supplier GetSupplier(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSupplier(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSupplier(int id, Supplier updatedSupplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSupplier(int id)
+        {
+            throw new NotImplementedException();
+        }  
+
+        public List<Product> GetProductsFromSupplier(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
