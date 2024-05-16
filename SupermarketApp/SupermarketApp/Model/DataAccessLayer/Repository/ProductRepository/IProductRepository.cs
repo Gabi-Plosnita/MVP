80985e502c91d379e19992ca02dfb544753d4fd6
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-16 18:43:23 +0300

    Create IProductRepository.cs

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/ProductRepository/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public interface IProductRepository
+    {
+       
+        List<Product> GetAllProducts();
+
+        List<Product> GetProductsByExpirationDate(DateTime expirationDate);
+
+        Product GetProductById(int id);
+
+        Product GetProductByName(string name);
+
+        Product GetProductByBarcode(string barcode);
+
+        void AddProduct(Product product);
+
+        void UpdateProduct(Product product);
+
+        void DeleteProduct(Product product);
+    }
+}
