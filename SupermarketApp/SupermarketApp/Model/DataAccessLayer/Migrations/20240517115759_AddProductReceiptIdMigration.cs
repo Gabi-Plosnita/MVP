ce8e3acd4a077767127a36977b47937a6053ef8a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-17 14:58:32 +0300

    Add migration for ProductRepositoryId

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Migrations/20240517115759_AddProductReceiptIdMigration.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupermarketApp.Model.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductReceiptIdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductReceipts",
+                table: "ProductReceipts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductReceiptId",
+                table: "ProductReceipts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductReceipts",
+                table: "ProductReceipts",
+                column: "ProductReceiptId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductReceipts_ReceiptId",
+                table: "ProductReceipts",
+                column: "ReceiptId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductReceipts",
+                table: "ProductReceipts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductReceipts_ReceiptId",
+                table: "ProductReceipts");
+
+            migrationBuilder.DropColumn(
+                name: "ProductReceiptId",
+                table: "ProductReceipts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductReceipts",
+                table: "ProductReceipts",
+                columns: new[] { "ReceiptId", "ProductId" });
+        }
+    }
+}
