fc3a4a54e164f34ec8f4ce60830776aba21736d9
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-17 12:41:28 +0300

    Create IStockRepository.cs

---
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/StockRepository/IStockRepository.cs
@@ -0,0 +1,16 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.DataAccessLayer.Repository
+{
+    public interface IStockRepository
+    {
+        List<Stock> GetStocks();
+
+        Stock GetStock(int id);
+
+        void AddStock(Stock stock);
+
+        List<Product> GetProducts(DateTime expirationDate);
+
+    }
+}
