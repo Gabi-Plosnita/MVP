250008634ef6a2e76edb8c652f56776449e82845
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-17 16:19:09 +0300

    Update IUserRepository.cs

--- SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/UserRepository/IUserRepository.cs
+++ SupermarketApp/SupermarketApp/Model/DataAccessLayer/Repository/UserRepository/IUserRepository.cs
@@ -4,6 +4,20 @@ namespace SupermarketApp.Model.DataAccessLayer.Repository
 {
     public interface IUserRepository
     {
+        // User Methods //
+        public User Login(string username, string password);
+
+        public List<User> GetUsers();
+
+        public User GetUser(int id);
+
+        public void AddUser(User user);
+
+        public void UpdateUser(int id, User user);
+
+        public void DeleteUser(int id);
+
+        // Recipts Methods //
         void AddReceipt(Receipt receipt);
 
         void AddProductReceipt(ProductReceipt productReceipt);
