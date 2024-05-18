87f097f3a1601ee811adf6d6012dd3904683cc52
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-18 15:16:22 +0300

    Update CategoryRequestDto.cs

--- SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/CategoryDtos/CategoryRequestDto.cs
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/CategoryDtos/CategoryRequestDto.cs
@@ -1,7 +1,10 @@
-﻿namespace SupermarketApp.Model.EntityLayer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
 {
     public class CategoryRequestDto
     {
+        [Required(ErrorMessage = "Name is required and can't be empty")]
         public string Name { get; set; }
     }
 }
