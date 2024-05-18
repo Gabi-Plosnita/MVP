15e8728621eebb96df45d3e60f3254c12d8f3521
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-18 15:45:01 +0300

    Create CategoryMapper.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Mappers/CategoryMapper/CategoryMapper.cs
@@ -0,0 +1,39 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Mappers
+{
+    public static class CategoryMapper
+    {
+        public static CategoryResponseDto MapToCategoryResponseDto(this Category category)
+        {
+           var categoryResponseDto = new CategoryResponseDto
+           {
+               CategoryId = category.CategoryId,
+               Name = category.Name,
+           };
+           
+           return categoryResponseDto;
+        }
+
+        public static List<CategoryResponseDto> MapToListOfCategoryResponseDto(this List<Category> categories)
+        {
+            var categoryResponseDtos = new List<CategoryResponseDto>();
+            foreach (var category in categories)
+            {
+                categoryResponseDtos.Add(category.MapToCategoryResponseDto());
+            }
+
+            return categoryResponseDtos;
+        }
+
+        public static Category MapToCategory(this CategoryRequestDto categoryRequestDto)
+        {
+            var category = new Category
+            {
+                Name = categoryRequestDto.Name,
+            };
+
+            return category;
+        }
+    }
+}
