17f6ef593e3e316253719d6602b918f01ee61baa
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-18 15:29:46 +0300

    Create ICategoryService.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Services
+{
+    public interface ICategoryService
+    {
+        List<CategoryResponseDto> GetCategories();
+
+        CategoryResponseDto GetCategory(int id);
+
+        void AddCategory(CategoryRequestDto category);
+
+        void UpdateCategory(int id, CategoryRequestDto updatedCategory);
+
+        void DeleteCategory(int id);
+
+        double GetTotalCategoryValue(int id);
+
+        List<ProductResponseDto> GetProductsFromCategory(int id);
+    }
+}
