99134e25ed5381de61ed4b3c642a12a39f084207
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-18 15:55:20 +0300

    Create ProductMapper.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Mappers/ProductMapper/ProductMapper.cs
@@ -0,0 +1,27 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Mappers
+{
+    public static class ProductMapper
+    {
+        public static ProductResponseDto MapToProductResponseDto(this Product product)
+        {
+            var productResponseDto = new ProductResponseDto
+            {
+                ProductId = product.ProductId,
+                Name = product.Name,
+                Barcode = product.Barcode,
+                CategoryName = product.Category.Name,
+                SupplierName = product.Supplier.Name
+            };
+
+            return productResponseDto;
+        }
+
+        public static List<ProductResponseDto> MapToListOfProductResponseDto(this List<Product> products)
+        {
+            return products.Select(product => MapToProductResponseDto(product)).ToList();
+        }
+
+    }
+}
