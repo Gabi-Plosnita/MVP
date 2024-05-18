99c4f1735cbb4db6c9ca1c5b372f1f6387c7cf4d
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-18 15:28:41 +0300

    Create ProductResponseDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ProductDtos/ProductResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public class ProductResponseDto
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Barcode { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string SupplierName { get; set; }
+    }
+}
