11701b4e11685fe1731d5f1e35ef0f9af27ea4f7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 15:27:22 +0300

    Create IUserService.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Services/UserService/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Services
+{
+    public interface IUserService
+    {
+        public UserResponseDto Login(string username, string password);
+
+        public List<UserResponseDto> GetUsers();
+
+        public UserResponseDto GetUser(int id);
+
+        public void AddUser(UserRequestDto userDto);
+
+        public void UpdateUser(int id, UserRequestDto updatedUser);
+
+        public void DeleteUser(int id);
+    }
+}
