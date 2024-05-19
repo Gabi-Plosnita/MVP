5cc331455a2c65e3d56c39e534e9479bb9119492
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 17:51:26 +0300

    Create ISupplierService.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Services/SupplierService/ISupplierService.cs
@@ -0,0 +1,19 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Services
+{
+    public interface ISupplierService
+    {
+        List<SupplierResponseDto> GetSuppliers();
+
+        SupplierResponseDto GetSupplier(int id);
+
+        void AddSupplier(SupplierRequestDto supplierDto);
+
+        void UpdateSupplier(int id, SupplierRequestDto updatedSupplierDto);
+
+        void DeleteSupplier(int id);
+
+        List<ProductResponseDto> GetProductsFromSupplier(int id);
+    }
+}
