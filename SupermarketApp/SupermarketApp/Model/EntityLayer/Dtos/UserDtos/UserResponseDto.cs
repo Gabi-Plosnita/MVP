3e447b3a515192c3f47a82cf70960c8a1b004990
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 15:23:03 +0300

    Create UserResponseDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/UserDtos/UserResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public class UserResponseDto
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public EUserType UserType { get; set; }
+
+    }
+}
