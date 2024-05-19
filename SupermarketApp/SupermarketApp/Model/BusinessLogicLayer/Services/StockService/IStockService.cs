eb2cf27170da8a4beb51e46a655fc218c1d65ae9
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 21:01:06 +0300

    Create IStockService.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Services/StockService/IStockService.cs
@@ -0,0 +1,15 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Services
+{
+    public interface IStockService
+    {
+        List<StockResponseDto> GetStocks();
+
+        StockResponseDto GetStock(int id);
+
+        void AddStock(StockRequestDto stockDto);
+
+        List<ProductResponseDto> GetProducts(DateTime expirationDate);
+    }
+}
