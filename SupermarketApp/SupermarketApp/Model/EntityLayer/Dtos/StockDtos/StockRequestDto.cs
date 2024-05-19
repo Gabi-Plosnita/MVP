8c57cf59591335bb769372dc7ed70a26d09277e9
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 20:31:32 +0300

    Create StockRequestDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/StockDtos/StockRequestDto.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
+{
+    public class StockRequestDto : IValidatableObject
+    {
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public EUnitType UnitType { get; set; }
+
+        public DateTime SupplyDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public double PurchasePrice { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(ProductId <= 0)
+            {
+                yield return new ValidationResult("Product Id must be greater than 0", new[] { nameof(ProductId) });
+            }
+
+            if(Quantity <= 0)
+            {
+                yield return new ValidationResult("Quantity must be greater than 0", new[] { nameof(Quantity) });
+            }
+
+            if(PurchasePrice <= 0)
+            {
+                yield return new ValidationResult("Purchase price must be greater than 0", new[] { nameof(PurchasePrice) });
+            }
+
+            if(SupplyDate >= ExpirationDate)
+            {
+                yield return new ValidationResult
+                    ("Supply date must be less than expiration date", new[] { nameof(SupplyDate), nameof(ExpirationDate) });
+            }
+        }
+    }
+}
