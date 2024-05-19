3e04aa40470143f5ff06356691a930551d3efec2
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 20:51:28 +0300

    Update SupplierRequestDto.cs

--- SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/SupplierDtos/SupplierRequestDto.cs
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/SupplierDtos/SupplierRequestDto.cs
@@ -2,13 +2,23 @@
 
 namespace SupermarketApp.Model.EntityLayer
 {
-    public class SupplierRequestDto
+    public class SupplierRequestDto : IValidatableObject
     {
-        [Required(ErrorMessage = "Name is required and can't be empty")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Country is required and can't be empty")]
         public string Country { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required", new[] { nameof(Name) });
+            }
+
+            if(string.IsNullOrEmpty(Country))
+            {
+                yield return new ValidationResult("Country is required", new[] { nameof(Country) });
+            }
+        }
     }
 }
