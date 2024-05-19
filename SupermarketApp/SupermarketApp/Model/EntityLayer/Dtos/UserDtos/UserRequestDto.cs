fb4010206a4131c7b1d4dc612910ed448f493b02
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 15:25:01 +0300

    Create UserRequestDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/UserDtos/UserRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
+{
+    public class UserRequestDto
+    {
+        [Required(ErrorMessage = "Username is required and can't be empty")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required and can't be empty")]
+        public string Password { get; set; }
+
+        public EUserType UserType { get; set; }
+    }
+}
