91be379d090b34b7deabe40fb15c0333f702b9f2
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 15:35:07 +0300

    Create UserMapper.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Mappers/UserMapper/UserMapper.cs
@@ -0,0 +1,32 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserResponseDto MapToUserResponseDto(this User user)
+        {
+            return new UserResponseDto
+            {
+                UserId = user.UserId,
+                Username = user.Username,
+                UserType = user.UserType
+            };
+        }
+
+        public static List<UserResponseDto> MapToListOfUserResponseDto(this List<User> users)
+        {
+            return users.Select(user => user.MapToUserResponseDto()).ToList();
+        }
+
+        public static User MapToUser(this UserRequestDto userDto)
+        {
+            return new User
+            {
+                Username = userDto.Username,
+                Password = userDto.Password,
+                UserType = userDto.UserType
+            };
+        }
+    }
+}
