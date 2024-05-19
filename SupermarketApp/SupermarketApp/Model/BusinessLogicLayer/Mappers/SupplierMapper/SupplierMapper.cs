5f92f5b5e183da4083694b5e81d403f02e77e3b7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 19:42:42 +0300

    Create SupplierMapper.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Mappers/SupplierMapper/SupplierMapper.cs
@@ -0,0 +1,32 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Mappers
+{
+    public static class SupplierMapper
+    {
+        public static SupplierResponseDto MapToSupplierResponseDto(this Supplier supplier)
+        {
+            return new SupplierResponseDto
+            {
+                SupplierId = supplier.SupplierId,
+                Name = supplier.Name,
+                Country = supplier.Country,
+            };
+        }
+
+        public static List<SupplierResponseDto> MapToListOfSupplierResponseDto(this List<Supplier> suppliers)
+        {
+            return suppliers.Select(supplier => supplier.MapToSupplierResponseDto()).ToList();
+        }
+
+        public static Supplier MapToSupplier(this SupplierRequestDto supplierDto)
+        {
+            return new Supplier
+            {
+                Name = supplierDto.Name,
+                Country = supplierDto.Country
+            };
+        }
+
+    }
+}
