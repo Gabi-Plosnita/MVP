affe13d6f8985fc17b36720367e5499114072bf8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 17:45:08 +0300

    Create Supplier dtos

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/SupplierDtos/SupplierRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
+{
+    public class SupplierRequestDto
+    {
+        [Required(ErrorMessage = "Name is required and can't be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Country is required and can't be empty")]
+        public string Country { get; set; }
+
+    }
+}
---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/SupplierDtos/SupplierResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public class SupplierResponseDto
+    {
+        public int SupplierId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+    }
+}
