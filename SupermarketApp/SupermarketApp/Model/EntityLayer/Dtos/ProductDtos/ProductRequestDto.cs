fb690b5c570fb4467359faa122b30b629d476e14
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 14:59:04 +0300

    Create ProductRequestDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ProductDtos/ProductRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
+{
+    public class ProductRequestDto
+    {
+        [Required(ErrorMessage = "Name is required and can't be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Barcode is required and can't be empty")]
+        public string Barcode { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int SupplierId { get; set; }
+
+    }
+}
