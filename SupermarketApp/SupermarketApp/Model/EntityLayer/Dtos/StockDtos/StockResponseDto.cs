a03ffbb2054af7a2591c7254b68490ca224a708c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 20:45:03 +0300

    Create StockResponseDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/StockDtos/StockResponseDto.cs
@@ -0,0 +1,22 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public class StockResponseDto
+    {
+        public int StockId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public EUnitType UnitType { get; set; }
+
+        public DateTime SupplyDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public double PurchasePrice { get; set; }
+
+        public double SalePrice { get; set; }
+
+    }
+}
