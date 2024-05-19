7ebcce84249c4e4d8bda841616d55f8490b1089a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 20:12:18 +0300

    Update ProductRequestDto.cs

--- SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ProductDtos/ProductRequestDto.cs
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ProductDtos/ProductRequestDto.cs
@@ -7,12 +7,18 @@ namespace SupermarketApp.Model.EntityLayer
         [Required(ErrorMessage = "Name is required and can't be empty")]
         public string Name { get; set; }
 
+
         [Required(ErrorMessage = "Barcode is required and can't be empty")]
         public string Barcode { get; set; }
 
+
+        [Required(ErrorMessage = "CategoryId is required and can't be empty")]
+        [Range(0, int.MaxValue, ErrorMessage = "Category Id must be at least 0")]
         public int CategoryId { get; set; }
 
-        public int SupplierId { get; set; }
 
+        [Required(ErrorMessage = "SupplierId is required and can't be empty")]
+        [Range(0, int.MaxValue, ErrorMessage = "Supplier Id is required and must be at least 0")]
+        public int SupplierId { get; set; }
     }
 }
