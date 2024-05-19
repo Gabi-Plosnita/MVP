2beb2099bdb6c125dc3ca045387de23696c244b7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 21:07:14 +0300

    Implement IStockService interface

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Services/StockService/StockService.cs
@@ -0,0 +1,57 @@
+﻿using SupermarketApp.Model.BusinessLogicLayer.Mappers;
+using SupermarketApp.Model.DataAccessLayer.Repository;
+using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Services
+{
+    public class StockService : IStockService
+    {
+        private readonly IStockRepository _stockRepository;
+
+        public StockService(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+
+        public List<StockResponseDto> GetStocks()
+        {
+            var stocks = _stockRepository.GetStocks();
+            var stockDtos = stocks.MapToListOfStockResponseDto();
+            return stockDtos;
+        }
+
+        public StockResponseDto GetStock(int id)
+        {
+            try
+            {
+                var stock = _stockRepository.GetStock(id);
+                var stockDto = stock.MapToStockResponseDto();
+                return stockDto;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void AddStock(StockRequestDto stockDto)
+        {
+            try
+            {
+                var stock = stockDto.MapToStock();
+                _stockRepository.AddStock(stock);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<ProductResponseDto> GetProducts(DateTime expirationDate)
+        {
+            var products = _stockRepository.GetProducts(expirationDate);
+            var productDtos = products.MapToListOfProductResponseDto();
+            return productDtos;
+        }
+    }
+}
