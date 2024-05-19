fa1297c17305ea95dd7ab357e5727d7cdf13a5f3
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-19 20:58:29 +0300

    Create StockMapper.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Mappers/StockMapper/StockMapper.cs
@@ -0,0 +1,40 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Mappers
+{
+    public static class StockMapper
+    {
+        public static StockResponseDto MapToStockResponseDto(this Stock stock)
+        {
+            return new StockResponseDto
+            {
+                StockId = stock.StockId,
+                ProductName = stock.Product.Name,
+                Quantity = stock.Quantity,
+                UnitType = stock.UnitType,
+                SupplyDate = stock.SupplyDate,
+                ExpirationDate = stock.ExpirationDate,
+                PurchasePrice = stock.PurchasePrice,
+                SalePrice = stock.SalePrice
+            };
+        }
+
+        public static List<StockResponseDto> MapToListOfStockResponseDto(this List<Stock> stocks)
+        {
+            return stocks.Select(stock => stock.MapToStockResponseDto()).ToList();
+        }
+
+        public static Stock MapToStock(this StockRequestDto stockRequestDto)
+        {
+            return new Stock
+            {
+                ProductId = stockRequestDto.ProductId,
+                Quantity = stockRequestDto.Quantity,
+                UnitType = stockRequestDto.UnitType,
+                SupplyDate = stockRequestDto.SupplyDate,
+                ExpirationDate = stockRequestDto.ExpirationDate,
+                PurchasePrice = stockRequestDto.PurchasePrice
+            };
+        }
+    }
+}
