5d95cdb9afe78398a1dec2180d090c3d9cf0f089
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 11:23:21 +0300

    Create ProductReceiptRequestDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ReceiptDtos/ProductReceiptRequestDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
+{
+    public class ProductReceiptRequestDto : IValidatableObject
+    {
+        public int ReceiptId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public EUnitType UnitType { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(ReceiptId <= 0)
+            {
+                yield return new ValidationResult("ReceiptId must be greater than 0", new[] { nameof(ReceiptId) });
+            }
+
+            if(ProductId <= 0)
+            {
+                yield return new ValidationResult("ProductId must be greater than 0", new[] { nameof(ProductId) });
+            }
+
+            if(Quantity <= 0)
+            {
+                yield return new ValidationResult("Quantity must be greater than 0", new[] { nameof(Quantity) });
+            }
+        }
+    }
+}
