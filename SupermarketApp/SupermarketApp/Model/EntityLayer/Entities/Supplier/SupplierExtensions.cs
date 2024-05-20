e9be81b9a38cf42d1c324c9a802396a12c8cfe87
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 22:58:25 +0300

    Create SupplierExtensions.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Entities/Supplier/SupplierExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public static class SupplierExtensions
+    {
+        public static void Update(this Supplier supplier, Supplier updatedSupplier)
+        {
+            supplier.Name = updatedSupplier.Name;
+            supplier.Country = updatedSupplier.Country;
+        }
+    }
+}
