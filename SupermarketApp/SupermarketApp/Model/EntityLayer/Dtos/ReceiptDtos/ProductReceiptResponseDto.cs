d9f3188461a0e8e4a8553749b31370a6c4b03ccb
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 11:28:09 +0300

    Create ProductReceiptResponseDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ReceiptDtos/ProductReceiptResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public class ProductReceiptResponseDto
+    {
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public EUnitType UnitType { get; set; }
+
+        public double Subtotal { get; set; }
+
+    }
+}
