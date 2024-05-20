86ffb68aad762c0dd9bf64ed61f4fd1f4a86b22a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 22:38:34 +0300

    Create BaseValidator.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/BaseValidator/BaseValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Model.EntityLayer
+{
+    public abstract class BaseValidator
+    {
+        public string GetValidationErrorMessage()
+        {
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(this, new ValidationContext(this), validationResults, true);
+
+            var errorMessage = string.Join(Environment.NewLine, validationResults.Select(vr => vr.ErrorMessage));
+            return errorMessage;
+        }
+    }
+}
