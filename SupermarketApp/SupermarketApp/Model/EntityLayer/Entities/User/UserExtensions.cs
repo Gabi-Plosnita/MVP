c8a736191981d52bb7e3da3e17f0298676a4889e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 23:01:35 +0300

    Create UserExtensions.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Entities/User/UserExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public static class UserExtensions
+    {
+        public static void Update(this User user, User updatedUser)
+        {
+            user.Username = updatedUser.Username;
+            user.Password = updatedUser.Password;
+            user.UserType = updatedUser.UserType;
+        }
+    }
+}
