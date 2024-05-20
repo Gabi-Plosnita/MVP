87f86d5555890bd40c9b99664a8b1438c48ee53e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 11:32:47 +0300

    Create ReceiptResponseDto.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Dtos/ReceiptDtos/ReceiptResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SupermarketApp.Model.EntityLayer
+{
+    public class ReceiptResponseDto
+    {
+        public int ReceiptId { get; set; }
+
+        public DateTime DateOfIssue { get; set; }
+
+        public string CashierName { get; set; }
+
+        public List<ProductReceiptResponseDto> ProductReceipts { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public bool IsPaid { get; set; }
+
+    }
+}
