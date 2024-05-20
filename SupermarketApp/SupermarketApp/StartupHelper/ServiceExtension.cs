b0406f13aded683cbeee02cb32cc0928ac055f09
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 12:27:29 +0300

    Configure all service in DI container

--- SupermarketApp/SupermarketApp/StartupHelper/ServiceExtension.cs
+++ SupermarketApp/SupermarketApp/StartupHelper/ServiceExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using SupermarketApp.Model.BusinessLogicLayer.Services;
 using SupermarketApp.Model.DataAccessLayer.DataContext;
 using SupermarketApp.Model.DataAccessLayer.Repository;
 
@@ -17,8 +18,6 @@ namespace SupermarketApp.StartupHelper
 
         public static void AddBusinessServices(this IServiceCollection services)
         {
-            services.AddTransient<IRepository, Repository>(); // delete this
-
             // Confiure Repoitories //
             services.AddTransient<BaseRepository>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
@@ -28,7 +27,13 @@ namespace SupermarketApp.StartupHelper
             services.AddTransient<ISupplierRepository, SupplierRepository>();
             services.AddTransient<IUserRepository, UserRepository>();
 
-            // Configure Services //   
+            // Configure Services //
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IReceiptService, ReceiptService>();
+            services.AddTransient<IStockService, StockService>();
+            services.AddTransient<ISupplierService, SupplierService>();
+            services.AddTransient<IUserService, UserService>();
         }
 
         public static void AddDatabaseServices(this IServiceCollection services, string connectionString)
