2a6eacae40e37d8210a09b1ccf422b108b1910f0
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 12:03:40 +0300

    Create ReceiptMapper.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Mappers/ReceiptMapper/ReceiptMapper.cs
@@ -0,0 +1,47 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Mappers
+{
+    public static class ReceiptMapper
+    {
+        public static ProductReceiptResponseDto MapToProductReceiptResponseDto(this ProductReceipt productReceipt)
+        {
+            return new ProductReceiptResponseDto
+            {
+                ProductName = productReceipt.Product.Name,
+                Quantity = productReceipt.Quantity,
+                UnitType = productReceipt.UnitType,
+                Subtotal = productReceipt.Subtotal
+            };
+        }
+
+        public static List<ProductReceiptResponseDto> MapToListOfProductReceiptResponseDto(this List<ProductReceipt> productReceipts)
+        {
+            return productReceipts.Select(pr => pr.MapToProductReceiptResponseDto()).ToList();
+        }
+
+        public static ReceiptResponseDto MapToReceiptResponseDto(this Receipt receipt)
+        {
+            return new ReceiptResponseDto
+            {
+                ReceiptId = receipt.ReceiptId,
+                DateOfIssue = receipt.DateOfIssue,
+                CashierName = receipt.Cashier.Username,
+                TotalPrice = receipt.TotalPrice,
+                IsPaid = receipt.IsPaid,
+                ProductReceipts = receipt.ProductReceipts.MapToListOfProductReceiptResponseDto()
+            };
+        }
+
+        public static ProductReceipt MapToProductReceipt(this ProductReceiptRequestDto productReceiptRequestDto)
+        {
+            return new ProductReceipt
+            {
+                ReceiptId = productReceiptRequestDto.ReceiptId,
+                ProductId = productReceiptRequestDto.ProductId,
+                Quantity = productReceiptRequestDto.Quantity,
+                UnitType = productReceiptRequestDto.UnitType
+            };
+        }
+    }
+}
