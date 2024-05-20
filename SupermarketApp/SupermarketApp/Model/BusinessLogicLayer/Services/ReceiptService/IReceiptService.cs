98109d1fa14259f2b79616acf6cc6b024c0f482d
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 11:53:17 +0300

    Create IReceiptService.cs

---
+++ SupermarketApp/SupermarketApp/Model/BusinessLogicLayer/Services/ReceiptService/IReceiptService.cs
@@ -0,0 +1,17 @@
+﻿using SupermarketApp.Model.EntityLayer;
+
+namespace SupermarketApp.Model.BusinessLogicLayer.Services
+{
+    public interface IReceiptService
+    {
+        int AddReceipt(int cashierId);
+
+        void AddProductReceipt(ProductReceiptRequestDto productReceiptDto);
+
+        void PayReceipt(int receiptId);
+
+        List<ReceiptResponseDto> GetReceipts();
+
+        ReceiptResponseDto GetReceipt(int receiptId);
+    }
+}
