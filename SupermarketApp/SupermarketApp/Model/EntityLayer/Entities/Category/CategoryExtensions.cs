f88ff7f2b0738303ef598308fe0c6ac360d8534f
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-20 22:24:26 +0300

    Create CategoryExtensions.cs

---
+++ SupermarketApp/SupermarketApp/Model/EntityLayer/Entities/Category/CategoryExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SupermarketApp.Model.EntityLayer.Entities
+{
+    public static class CategoryExtensions
+    {
+        public static void UpdateCategory(this Category category, Category updatedCategory)
+        {
+            category.Name = updatedCategory.Name;
+        }
+    }
+}
