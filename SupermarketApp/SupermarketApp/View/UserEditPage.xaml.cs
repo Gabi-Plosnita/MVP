c3afcd306429eb8e9fd249ba52c94493efd632db
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-21 23:26:21 +0300

    Create UserEdit page

---
+++ SupermarketApp/SupermarketApp/View/UserEditPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketApp.Model.EntityLayer;
+using SupermarketApp.ViewModel;
+using System.Windows.Controls;
+
+namespace SupermarketApp.View
+{
+    public partial class UserEditPage : Page
+    {
+        public UserEditPage()
+        {
+            InitializeComponent();
+            DataContext = new UserEditViewModel();
+        }
+
+        public UserEditPage(UserResponseDto user)
+        {
+            InitializeComponent();
+            DataContext = new UserEditViewModel(user);
+        }
+    }
+}
