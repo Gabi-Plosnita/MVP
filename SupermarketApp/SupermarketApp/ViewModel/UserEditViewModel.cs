6b36d5b8fe9a560c3c208b3be585fb3ac269817d
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-21 23:26:10 +0300

    Create UserEditViewModel.cs

---
+++ SupermarketApp/SupermarketApp/ViewModel/UserEditViewModel.cs
@@ -0,0 +1,101 @@
+﻿using SupermarketApp.Commands;
+using SupermarketApp.Model.BusinessLogicLayer.Mappers;
+using SupermarketApp.Model.BusinessLogicLayer.Services;
+using SupermarketApp.Model.EntityLayer;
+using SupermarketApp.View;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SupermarketApp.ViewModel
+{
+    public class UserEditViewModel : BaseViewModel
+    {
+        private readonly IUserService _userService = new UserService();
+
+        private bool _isEditMode;
+        public UserResponseDto EditingUser { get; set; }
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        public ObservableCollection<string> Roles { get; } = new ObservableCollection<string>()
+        {
+            "Admin",
+            "User"
+        };
+        public string SelectedRole { get; set; }
+        public ICommand SaveUserCommand { get; set; }
+
+        public string PageTitle
+        {
+            get
+            {
+                if (_isEditMode)
+                {
+                    return "Edit Category";
+                }
+                else
+                {
+                    return "Add Category";
+                }
+            }
+        }
+
+        public UserEditViewModel()
+        {
+
+            SaveUserCommand = new RelayCommand<object>(SaveUser);
+            _isEditMode = false;
+        }
+
+        public UserEditViewModel(UserResponseDto user) : this()
+        {
+            Username = user.Username;
+            Password = user.Password;
+            SelectedRole = user.UserType == EUserType.Admin ? "Admin" : "User";
+            _isEditMode = true;
+            EditingUser = user;
+        }
+
+        private void SaveUser(object? obj)
+        { 
+            var userRequestDto = new UserRequestDto()
+            {
+                Username = Username,
+                Password = Password,
+                UserType = SelectedRole == "Admin" ? EUserType.Admin : EUserType.Cashier
+            };
+            if (_isEditMode)
+            {      
+                try
+                {
+                    int id = EditingUser.UserId;
+                    _userService.UpdateUser(id, userRequestDto);
+                    MessageBox.Show("User updated successfully!");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+            else
+            {
+                try
+                {
+                    _userService.AddUser(userRequestDto);
+                    MessageBox.Show("User added successfully!");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+            var currentPage = obj as Page;
+            var adminPage = new AdminPage();
+            currentPage?.NavigationService?.Navigate(adminPage);
+        }
+    }
+}
