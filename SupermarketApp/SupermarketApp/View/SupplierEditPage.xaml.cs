03e00f1bf84711ecf0c5ca6d073bb00ba869380c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-22 02:51:10 +0300

    Create SupplierEdit page

---
+++ SupermarketApp/SupermarketApp/View/SupplierEditPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketApp.Model.EntityLayer;
+using SupermarketApp.ViewModel;
+using System.Windows.Controls;
+
+namespace SupermarketApp.View
+{
+    public partial class SupplierEditPage : Page
+    {
+        public SupplierEditPage()
+        {
+            InitializeComponent();
+            DataContext = new SupplierEditViewModel();
+        }
+
+        public SupplierEditPage(SupplierResponseDto supplier)
+        {
+            InitializeComponent();
+            DataContext = new SupplierEditViewModel(supplier);
+        }
+    }
+}
