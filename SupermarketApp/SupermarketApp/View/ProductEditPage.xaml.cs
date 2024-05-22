5d3eee56f22b682f19906a13a18774f03ea8af8f
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-22 12:20:28 +0300

    Create ProductEdit page

---
+++ SupermarketApp/SupermarketApp/View/ProductEditPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketApp.Model.EntityLayer;
+using SupermarketApp.ViewModel;
+using System.Windows.Controls;
+
+namespace SupermarketApp.View
+{
+    public partial class ProductEditPage : Page
+    {
+        public ProductEditPage()
+        {
+            InitializeComponent();
+            DataContext = new ProductEditViewModel();
+        }
+
+        public ProductEditPage(ProductResponseDto product)
+        {
+            InitializeComponent();
+            DataContext = new ProductEditViewModel(product);
+        }
+    }
+}
